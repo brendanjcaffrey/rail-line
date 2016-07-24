a9f11c4b3cfc1ba6488db552035e017b96857bae
Brendan Caffrey <brendan@jcaffrey.com>
2016-07-24 14:49:42 -0500

    add lat/long info for stations

--- update_stations.rb
+++ update_stations.rb
@@ -7,10 +7,9 @@ print 'Enter XML file URL (from Export): ' ; STDOUT.flush
 
 file = STDIN.gets.chomp
 contents = Net::HTTP.get(URI.parse(file))
-contents = File.read('rows.xml')
 doc = Nokogiri::XML(contents)
 
-Station = Struct.new(:id, :name, :desc_name)
+Station = Struct.new(:id, :name, :desc_name, :latitude, :longitude)
 id_station_map = {}
 name_counts = {}
 
@@ -20,7 +19,10 @@ doc.css('row').each do |row|
 
   name = row.at_css('station_name').content
   desc_name = row.at_css('station_descriptive_name').content
-  station = Station.new(id, name, desc_name)
+  location = row.at_css('location')
+  latitude = location['latitude']
+  longitude = location['longitude']
+  station = Station.new(id, name, desc_name, latitude, longitude)
   id_station_map[id] = station
 
   name_counts[name] ||= 0
@@ -28,7 +30,7 @@ doc.css('row').each do |row|
 end
 
 stations = id_station_map.values.sort_by { |station| station.desc_name }
-new_file = "Station = Struct.new(:id, :name)
+new_file = "Station = Struct.new(:id, :name, :latitude, :longitude)
 class CTAInfo
   class << self ; attr_reader :stations ; end
 
@@ -38,7 +40,7 @@ class CTAInfo
 
 stations.each do |station|
   name = name_counts[station.name] > 1 ? station.desc_name : station.name
-  new_file << "\n    #{station.id} => Station.new(#{station.id}, \"#{name}\"),"
+  new_file << "\n    #{station.id} => Station.new(#{station.id}, \"#{name}\", #{station.latitude}, #{station.longitude}),"
 end
 
 new_file = new_file[0..-2] # remove the trailing comma
